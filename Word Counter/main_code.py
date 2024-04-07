d421a4e520b58b5a84f2e824f749b3e637d5b72b
Srijan Kumar <noob123srijan@gmail.com>
2024-04-07 18:37:58 +0530

    I forgot to Add word counter last time lol XD

--- Word Counter/main_code.py
+++ Word Counter/main_code.py
@@ -1,12 +1,13 @@
 def main():
     print("Welcome to the Program!")
     msg = input("Please enter your passage:")
+    conveter(msg)
     counter(msg.lower())
 
 def counter(msg):
-    words , vowels , const , spaces , special_char , numerical_char, dots = 0,0,0,0,0,0,0
+    character , vowels , const , spaces , special_char , numerical_char, dots = 0,0,0,0,0,0,0
     for i in msg:
-        words += 1
+        character += 1
         if i in ['a','i','e','o','u']:
             vowels += 1
         elif i == ' ':
@@ -19,8 +20,15 @@ def counter(msg):
             numerical_char += 1
         else :
             const += 1  
-    print(f'''number of words: {words}\nnumber of vowels: {vowels}\nnumber of consonants: {const}\nnumber of dots: {dots}
+    print(f'''number of characters: {character}\nnumber of vowels: {vowels}\nnumber of consonants: {const}\nnumber of dots: {dots}
     number of spaces: {spaces}\nnumber of special characters: {special_char}\nnumber of numerical values:{numerical_char}''')
     print("Thank you for using me")
+
+def conveter(message):
+    words = 0
+    for i in message:
+        if i.isspace():
+            words += 1
+    print(f"number of words are: {words}")
 
 main()

020d8c2f1bc38a2513900ca81539b60ec1ec46bd
Srijan Kumar <noob123srijan@gmail.com>
2024-04-11 21:03:43 +0530

    Simplified the code, but this time it works

--- Python-Projects/Password manager/pass_manager.py
+++ Python-Projects/Password manager/pass_manager.py
@@ -1,72 +1,34 @@
 from cryptography.fernet import Fernet
 
 def main():
-    master_key = "Lucifer"
-    for i in range(4):
-        master_pass = input("Enter the master password : ").strip()
-        if master_pass == "EX":
-            print("You are exiting now ....")
-            break
-        elif master_pass != master_key: 
-            print(f"WRONG PASSWORD !! ")
-            if i != 3:
-                print(f"PLEASE TRY A DIFFERENT OPTION \nyou have {3 - int(i)} chances left")
-            else:
-                print("GET THE FCK OUT OF HERE")
-        else:
-            run(master_key)
-
-def run(master_key):
-    genrate_key()
-    key = load_key()
-    fer = Fernet(key)
-    user_wants = input("Do you want to add a password or view your saved passwords ? (view/add) ").lower()
-    if user_wants == "add":
-        add_pass(fer)
-    elif user_wants == "view":
-        view_pass(fer)
-    else:
-        print("Invalid option")
-
-def view_pass(fer):
-    '''
-    this block of code is not working
-    and I have no more clue in how to
-    actually solve it. So I'll just leave it
-    here for now, I'll come back on it later 
-    in the future.Goodbye
-                          ~ the-coder-devil
-                            07 Apr 2024 16:37 
-    '''
-
     print("Welcome to Password Manager")
+    user = input("Do you want to add password or view saved ones: ").lower().strip()
+    if user == "add":
+        user_name = input("Enter your username : ")
+        password = input("Enter your Password : ")
+        add_pass(user_name,password)
+    elif user == "view":
+        user_name = input("Enter your username : ")
+        view_pass(user_name)
+    else:
+        print('Enter valid option!')
+
+def view_pass(user_name: str):
     num = 0
     with open("password.txt",'r') as Souce:
         for line in Souce.readlines():
             num += 1
-            username , passw = line.split("|")
-            print(f"#{num} Username: {username} Password: {fer.decrypt(passw.encode()).decode()}")
+            if line.startswith(user_name):
+                _, passw,key = line.split("|")
+                fernet = Fernet(key)
+                print(f"#{num} Username: {user_name} Password: {fernet.decrypt(passw).decode()}")
 
-def add_pass(fer):
-    print("Welcome to Password Manager")
-    user_name = input("Enter your username : ")
-    password = input("Enter your Password : ")
+def add_pass(user_name: str, password:str):
     
     with open("password.txt",'a') as Source:
-        Source.write(user_name+"|"+fer.encrypt(password.encode()).decode()+"\n")
+        key = Fernet.generate_key()
+        fernet = Fernet(key)
+        Source.write(user_name + "|" + fernet.encrypt(password.encode()).decode()+ "|" + key.decode() + "\n")
         print("the details has been added succesfully!")
-        print("Enter 'EX' to exit")
-
-def genrate_key():
-    key = Fernet.generate_key()
-    with open("key.key",'wb') as key_file:
-        key_file.write(key)
-
-
-def load_key():
-    file = open("key.key",'rb')
-    key = file.read()
-    file.close()
-    return key
 
 main()
